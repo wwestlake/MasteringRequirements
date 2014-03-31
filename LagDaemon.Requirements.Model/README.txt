c27ab2ca1855af55bdf0b95a0ff750f37c530b7e
wwestlake <wwestlake@lagdaemon.com>
2014-03-30 22:41:32 -0400

    Initial Layout

---
+++ LagDaemon.Requirements.Model/README.txt
@@ -0,0 +1,5 @@
+﻿This library contains the application data model for Matering Requirements which
+is an object oriented data management system for managing and processing reuiquirements
+for any type of system under development.
+
+
---
+++ LagDaemon.Requirements.Model/Requirement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagDaemon.Requirements.Model
+{
+    public class Requirement
+    {
+        public string Identity { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+    }
+}
