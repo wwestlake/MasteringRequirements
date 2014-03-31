0088804df78fa0765136fa34da280a1d65534f03
wwestlake <wwestlake@lagdaemon.com>
2014-03-30 23:16:50 -0400

    Modified Requirement class

--- LagDaemon.Requirements.Model/Requirement.cs
+++ LagDaemon.Requirements.Model/Requirement.cs
@@ -12,6 +12,6 @@ namespace LagDaemon.Requirements.Model
         public string Name { get; set; }
         public string Text { get; set; }
         public DateTime Created { get; set; }
-
+        public DateTime Modified { get; set; }
     }
 }
